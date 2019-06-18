de6ebccb39fa59b503d029433cb6c3f9fea16822
Amber Joseph <jamber919@gmail.com>
2019-06-18 13:02:45 +1200

    Initial test commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Stellar by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
+    title: "Amber Joseph",
+    author: "Amber Joseph",
+    description: "A personal professional porfolio of Software and other work."
   },
   plugins: [
     'gatsby-plugin-react-helmet',
