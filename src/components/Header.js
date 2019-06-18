26088b988adae07958da7301cf3f901a6ade3458
Amber Joseph <jamber919@gmail.com>
2019-06-18 16:26:17 +1200

    Added duck logo and opacity fade on hover

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/logo.png';
+import logoWink from '../assets/images/logo_wink.png';
 
 const Header = (props) => (
     <header id="header" className="alt">
         <span className="logo"><img src={logo} alt="" /></span>
+        <span className="overlay"><img src={logoWink} alt=""/></span>
         <h1>AMBER JOSEPH</h1>
         <p>Just another free, fully responsive site template<br />
         design by <a href="https://html5up.net">HTML5 UP</a>.</p>
