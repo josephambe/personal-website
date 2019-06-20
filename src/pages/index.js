533b8d7462304e14d45dcaaebaad1b0299716a3c
Amber Joseph <jamber919@gmail.com>
2019-06-20 17:32:16 +1200

    Fixed buttons, text, and general layouts for Landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,9 +49,10 @@ class Index extends React.Component {
                 <header className="major">
                   <h2>WHY SOFTWARE</h2>
                 </header>
-                  <p> Originally, the plan was to study Fine Arts and Painting. Instead, one afternoon in 2016, I announced to a shocked family audience that I was headed to Victoria University of Wellington to pursue a degree in Software Engineering.</p>
-                  <p>I had realised coding is the most effective way to scale and share ideas with the world.</p>
-                  <p>And despite the perceived ‘I’m a coder’ image, I was thrilled to discover that computer science is not only a great creative outlet, but still let me work in teams, pitch ideas that excite me, and ultimately continue writing a lifelong plot to improve the world.</p>
+                <p>Originally, the plan was to <h2>study Fine Arts and Painting.</h2>Instead, one afternoon in 2016, I announced to a shocked family audience that I was headed to Victoria University of Wellington to pursue a degree in Software Engineering.
+                  < br />I had realised coding is the most effective way to <h2>scale and share ideas with the world. </h2>
+                And despite the perceived ‘I’m a coder’ image, I was thrilled to discover that computer science is not only a great creative outlet, but still let me work in teams, pitch ideas that excite me, and ultimately continue writing a lifelong plot to improve the world.
+                </p>
                 {/*<ul className="actions">*/}
                   {/*<li><Link to="/generic" className="button">Learn More</Link></li>*/}
                 {/*</ul>*/}
@@ -60,38 +61,37 @@ class Index extends React.Component {
             </div>
           </section>
 
-          <section id="first" className="main special">
-            <header className="major">
-              <h2>WHERE NEXT</h2>
-            </header>
-            <ul className="features">
-              <li>
-                <span className="icon major style1 fa-code"></span>
-                <h3>Ipsum consequat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
-              </li>
-              <li>
-                <span className="icon major style3 fa-copy"></span>
-                <h3>Amed sed feugiat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
-              </li>
-              <li>
-                <span className="icon major style5 fa-diamond"></span>
-                <h3>Dolor nullam</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
-              </li>
-            </ul>
-            <footer className="major">
-              <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
-              </ul>
-            </footer>
-          </section>
+          {/*<section id="first" className="main special">*/}
+            {/*<header className="major">*/}
+              {/*<h2>WHERE NEXT</h2>*/}
+            {/*</header>*/}
+            {/*<ul className="features">*/}
+              {/*<li>*/}
+                {/*<span className="icon major style1 fa-code"></span>*/}
+                {/*<h3>Ipsum consequat</h3>*/}
+                {/*<p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>*/}
+              {/*</li>*/}
+              {/*<li>*/}
+                {/*<span className="icon major style3 fa-copy"></span>*/}
+                {/*<h3>Amed sed feugiat</h3>*/}
+                {/*<p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>*/}
+              {/*</li>*/}
+              {/*<li>*/}
+                {/*<span className="icon major style5 fa-diamond"></span>*/}
+                {/*<h3>Dolor nullam</h3>*/}
+                {/*<p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>*/}
+              {/*</li>*/}
+            {/*</ul>*/}
+            {/*<footer className="major">*/}
+              {/*<ul className="actions">*/}
+                {/*<li><Link to="/generic" className="button">Learn More</Link></li>*/}
+              {/*</ul>*/}
+            {/*</footer>*/}
+          {/*</section>*/}
 
           <section id="second" className="main special">
             <header className="major">
               <h2>WHAT NOW</h2>
-              <p>STUDY</p>
             </header>
             {/*<ul className="statistics">*/}
               {/*<li className="style1">*/}
@@ -116,14 +116,20 @@ class Index extends React.Component {
               {/*</li>*/}
             {/*</ul>*/}
             <p className="content">
+              <header className="major">
+                <p>STUDY</p>
+              </header>
               • Studying towards a Bachelor of Software Engineering at Victoria University of Wellington. < br />< br />
               • Graduation date Nov, 2020.< br />< br />
-              • Spent one year studying at Asia’s highest ranked University.  Read more.< br />< br />
+              • Spent one year studying at Asia’s highest ranked University.  Read more.< br />
               <header className="major">
                 <p>TECHNICAL</p>
               </header>
-              Outside of my studies, I have been independently improving my technical skills by participating in various hackathons and personal projects.< br />
-              I have experience using languages such as Java, HTTP, C, C+, Python, and React.< br />< br />
+              Outside of my studies, I have been independently improving my technical skills by participating in various hackathons and personal projects.< br />< br />
+              I have experience using languages such as Java, HTTP, C, Python, and React.< br />< br />
+              <ul className="actions">
+                <li><Link to="/generic" className="button">My Hardest Project to Date</Link></li>
+              </ul>
               <header className="major">
                 <p>AWARDS</p>
               </header>
@@ -138,9 +144,7 @@ class Index extends React.Component {
 
             </p>
             <footer className="major">
-              <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
-              </ul>
+
             </footer>
           </section>
 
@@ -152,17 +156,24 @@ class Index extends React.Component {
                 </header>
                 <span className="image"><img src={pic02} alt="" /></span>
               </div>
-              <header className="major">
-                <p>CAREER GOAL</p>
-              </header>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+              <div className="content">
+                <header className="major">
+                  <p>CAREER GOAL</p>
+                </header>
+                <p>Demystify technology and bridge the gap between IT and business strategy by providing innovative solutions that build better companies.</p><br />
+                <header className="major">
+                  <p>I'M EXCITED ABOUT</p>
+                </header>
+                <p>1. The future of Space! In 2018 I co-founded…..oh you want to know more? Read about it here.<br /><br />
+                  2. UI and UX design process is my current obsession. Yummy designs and intuitive use. See my latest project in this area!<br /><br />
+                  3. Also Microsoft recently announced they are storying big data in DNA?!</p>
+              </div>
             </div>
 
             <footer className="major">
               <ul className="actions">
-                <li><Link to="/generic" className="button special">Get Started</Link></li>
-                <li><Link to="/generic" className="button">Learn More</Link></li>
+                {/*<li><Link to="/generic" className="button special">Get Started</Link></li>*/}
+                {/*<li><Link to="/generic" className="button">Learn More</Link></li>*/}
               </ul>
             </footer>
           </section>
