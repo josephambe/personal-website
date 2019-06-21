d0d5e5d64a364186ef0755bc5e4eff04f14759ed
Amber Joseph <jamber919@gmail.com>
2019-06-21 15:28:48 +1200

    Added working links to social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,10 +16,10 @@ const Footer = (props) => (
               <li><span className="label">CONTACT</span></li>
             </ul>
             <ul className="icons">
-                <li><a href="#" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon fa-envelope alt"><span className="label">Mail</span></a></li>
-                <li><a href="#" className="icon fa-linkedin alt"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://twitter.com/amberkjoseph?lang=en" className="icon fa-twitter alt"><span className="label">Twitter</span></a></li>
+                <li><a href="https://github.com/josephambe" className="icon fa-github alt"><span className="label">GitHub</span></a></li>
+                <li><a href="mailto:jamber919@gmail.com?subject=Let's catch up!" className="icon fa-envelope alt"><span className="label">Mail</span></a></li>
+                <li><a href="https://www.linkedin.com/in/amber-joseph/" className="icon fa-linkedin alt"><span className="label">LinkedIn</span></a></li>
             </ul>
         </section>
         <p className="copyright">&copy; Amber Joseph 2019. Built with <a href="https://www.gatsbyjs.org/">Gatsby</a> and React.</p>
