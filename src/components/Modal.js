ad34e824b32c7486764ab14d5a2fa01e2780ecc0
Amber Joseph <jamber919@gmail.com>
2019-06-22 13:50:55 +1200

    Got overlay to be a circle and change background colour using 'contentStyle'

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,18 +3,17 @@ import Popup from 'reactjs-popup'
 import Header from './Header'
 
 const Modal = (props) => (
-  <Popup trigger={<button className="button"> Open Modal </button>} modal>
+  <Popup       contentStyle={{ background: "rgba(0,255,255,0.98", borderRadius: "50%", height: "800px", width: "800px", textAlign: "center", text: "10px" }}
+               trigger={<button className="button"> Open Modal </button>} modal>
     {close => (
       <div className="modal">
         <a className="close" onClick={close}>
           &times;
         </a>
-        <div className="header"> Modal Title </div>
+        <div className="header"> WHY THE DUCK? </div>
         <div className="content">
           {' '}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+          Because it's cute
           <br />
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
           commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
