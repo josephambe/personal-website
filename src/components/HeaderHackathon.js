964f35ea909f157cfb711829b7ac474b0802522b
Amber Joseph <jamber919@gmail.com>
2019-06-24 11:41:31 +1200

    Changed title and definition in header

--- src/components/HeaderHackathon.js
+++ src/components/HeaderHackathon.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const HeaderHackathon = (props) => (
     <header id="header">
-        <h1>Generic</h1>
-        <p>Ipsum dolor sit amet nullam</p>
+        <h1>HACKATHONS</h1>
+        <h2>(noun)   /ˈhakəθɒn/ </h2>
+        <h3>An event, typically lasting several days, in which a large number of people meet to engage in collaborative computer programming. Used by non-techy people to get attention and sound cool. Because let’s be real, it’s a cool word.</h3>
     </header>
+
 )
 
 export default HeaderHackathon
