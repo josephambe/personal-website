a057aa5d5641e5407534086d9b18ca757209b2db
Amber Joseph <jamber919@gmail.com>
2019-06-23 14:33:36 +1200

    Clicking outside the pop up menu exits the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,32 @@
 import React from "react";
 import Popup from "reactjs-popup"
 
+const contentStyle = {
+  background: "rgba(255,255,255,0)",
+  // width: "80%",
+  padding: "30px",
+  border: "none"
+};
+
 const Menu = (props) => (
   <Popup
-    modal
-    overlayStyle={{ background: "rgba(255,255,255,0.98" }}
-    closeOnDocumentClick={false}
-    trigger={<ul className="burger">
-      <a href="#" className="icon fa-bars alt"><span className="label">Burger Menu</span></a>
-    </ul>}
+    contentStyle={{ background: "rgb(238,238,238)", borderRadius: "50%", height: "800px", width: "800px", paddingTop: "100px", textAlign: "center", zIndex: "9999"}}
+    overlayStyle={{ paddingTop: "80px"}}
+    // closeOnDocumentClick={false}
+    trigger={
+      <ul className="burger">
+        <a href="#" className="icon fa-bars alt"><span className="label">Burger Menu</span></a>
+      </ul>
+    } menu
   >
     {close => (
       <div className="menu">
-        <ul>
-          <li onClick={close}>Home</li>
-          <li onClick={close}>Getting Started</li>
-          <li onClick={close}>Component API</li>
-          <li onClick={close}>Use Case - Tooltip</li>
-          <li onClick={close}>Use Case - Modal</li>
-          <li onClick={close}>Use Case - Menu</li>
-          <li onClick={close}>Contributing</li>
+        <ul className="actions">
+            <li>PROJECTS</li>< br/>
+            <li>HACKATHONS</li>< br/>
+            <li>SPOTTED</li>< br/>
+            <li>CONTACT</li>
+          {/*<li onClick={close}>CONTACT</li>*/}
         </ul>
       </div>
     )}
