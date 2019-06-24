e09c48442b98d87a099b6f5321c3458a98feb6cc
Amber Joseph <jamber919@gmail.com>
2019-06-24 12:16:30 +1200

    Added home logo with temp image

--- src/components/HeaderHackathon.js
+++ src/components/HeaderHackathon.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from '../assets/images/logo.png'
 
 const HeaderHackathon = (props) => (
+
     <header id="header">
-        <h1>HACKATHONS</h1>
+      <figure className="homo-logo">
+        <img className="home-logo" src={logo} alt=""/>
+      </figure>
+      <h1>HACKATHONS</h1>
         <h2>(noun)   /ˈhakəθɒn/ </h2>
         <h3>An event, typically lasting several days, in which a large number of people meet to engage in collaborative computer programming. Used by non-techy people to get attention and sound cool. Because let’s be real, it’s a cool word.</h3>
     </header>
