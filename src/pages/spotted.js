46a7a60bf63b0d34aa658b502d0a8714efab5a13
Amber Joseph <jamber919@gmail.com>
2019-06-25 17:30:54 +1200

    Added hover buttons

--- src/pages/spotted.js
+++ src/pages/spotted.js
@@ -10,31 +10,6 @@ import pic04 from '../assets/images/pic04.jpg'
 import HeaderHackathon from '../components/HeaderHackathon'
 
 
-// const IMAGES =
-//   [{
-//     src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-//     thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg",
-//     // thumbnailWidth: 320,
-//     // thumbnailHeight: 174,
-//     // isSelected: true,
-//     caption: "After Rain (Jeshu John - designerspics.com)"
-//   },
-//     {
-//       src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-//       thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_n.jpg",
-//       // thumbnailWidth: 320,
-//       // thumbnailHeight: 212,
-//       tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
-//       caption: "Boats (Jeshu John - designerspics.com)"
-//     },
-//
-//     {
-//       src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-//       thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_n.jpg",
-//       // thumbnailWidth: 320,
-//       // thumbnailHeight: 212
-//     }]
-
 class Spotted extends React.Component {
   constructor(props){
     super(props);
@@ -114,31 +89,23 @@ Spotted.propTypes = {
 };
 
 const captionStyle = {
-  backgroundColor: "rgba(0, 0, 0, 0.8)",
+  backgroundColor: "#FFC417",
+  textAlign: "center",
   maxHeight: "240px",
   overflow: "hidden",
+  align: "center",
   position: "absolute",
-  bottom: "0",
-  width: "100%",
+  bottom: "45px",
+  left: "10px",
+  right: "10px",
+  width: "90%",
   color: "white",
-  padding: "2px",
-  fontSize: "90%"
+  // padding: "20px",
+  fontSize: "90%",
+  borderRadius: "25px",
+  // paddingBottom: "35px"
 };
 
-const customTagStyle = {
-  wordWrap: "break-word",
-  display: "inline-block",
-  backgroundColor: "white",
-  height: "auto",
-  fontSize: "75%",
-  fontWeight: "600",
-  lineHeight: "1",
-  padding: ".2em .6em .3em",
-  borderRadius: ".25em",
-  color: "black",
-  verticalAlign: "baseline",
-  margin: "2px"
-};
 
 Spotted.defaultProps = {
   images: ([
@@ -147,65 +114,65 @@ Spotted.defaultProps = {
       thumbnail: "https://c7.staticflickr.com/9/8106/28941228886_86d1450016_n.jpg",
       thumbnailWidth: 271,
       thumbnailHeight: 320,
-      tags: [{value: "Nature", title: "Nature | Flowers"}],
-      caption: "Orange Macro (Tom Eversley - isorepublic.com)"
+      // tags: [{value: "Nature", title: "Nature | Flowers"}],
+      caption: "SINGAPORE"
     },
     {
       src: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
       thumbnail: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_n.jpg",
       thumbnailWidth: 320,
       thumbnailHeight: 190,
-      tags: [{value: "Architecture", title: "Architecture | Outdoors"},
-        {value: "Industrial", title: "Industrial"}],
-      caption: "286H (gratisography.com)"
+      // tags: [{value: "Architecture", title: "Architecture | Outdoors"},
+      //   {value: "Industrial", title: "Industrial"}],
+      caption: "MALAYSIA"
     },
     {
       src: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
       thumbnail: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_n.jpg",
       thumbnailWidth: 320,
       thumbnailHeight: 148,
-      tags: [{value: "People", title: "People"},
-        {value: "Architecture", title: "Architecture | Outdoors"},
-        {value: "Industrial", title: "Industrial"}],
-      caption: "315H (gratisography.com)"
+      // tags: [{value: "People", title: "People"},
+      //   {value: "Architecture", title: "Architecture | Outdoors"},
+      //   {value: "Industrial", title: "Industrial"}],
+      caption: "THAILAND"
     },
     {
       src: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
       thumbnail: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_n.jpg",
       thumbnailWidth: 320,
       thumbnailHeight: 213,
-      caption: "201H (gratisography.com)"
+      caption: "MALAYSIA"
     },
     {
       src: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_b.jpg",
       thumbnail: "https://c2.staticflickr.com/9/8239/28897202241_1497bec71a_n.jpg",
       thumbnailWidth: 248,
       thumbnailHeight: 320,
-      caption: "Big Ben (Tom Eversley - isorepublic.com)"
+      caption: "CAMBODIA"
     },
     {
       src: "https://c1.staticflickr.com/9/8785/28687743710_870813dfde_h.jpg",
       thumbnail: "https://c1.staticflickr.com/9/8785/28687743710_3580fcb5f0_n.jpg",
       thumbnailWidth: 320,
       thumbnailHeight: 113,
-      tags: [{value: "People", title: "People"},
-        {value: "Industrial", title: "Industrial"}],
-      caption: "Red Zone - Paris (Tom Eversley - isorepublic.com)"
+      // tags: [{value: "People", title: "People"},
+      //   {value: "Industrial", title: "Industrial"}],
+      caption: "JAPAN"
     },
     {
       src: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_b.jpg",
       thumbnail: "https://c6.staticflickr.com/9/8520/28357073053_cafcb3da6f_n.jpg",
       thumbnailWidth: 313,
       thumbnailHeight: 320,
-      caption: "Wood Glass (Tom Eversley - isorepublic.com)"
+      caption: "BALI"
     },
     {
       src: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_b.jpg",
       thumbnail: "https://c8.staticflickr.com/9/8104/28973555735_ae7c208970_n.jpg",
       thumbnailWidth: 320,
       thumbnailHeight: 213,
-      tags: [{value: "Nature", title: "Nature | Flowers"}],
-      caption: "Flower Interior Macro (Tom Eversley - isorepublic.com)"
+      // tags: [{value: "Nature", title: "Nature | Flowers"}],
+      caption: "WORKSHOP"
     }
   ])
 };
