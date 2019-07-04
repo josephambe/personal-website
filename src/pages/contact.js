0f39a5bbebdc5a985032f78abda262014cd732ee
Amber Joseph <jamber919@gmail.com>
2019-07-05 11:38:03 +1200

    Added hover effect to icons

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,8 +24,8 @@ class Contact extends React.Component {
                 <div className="grid-item">
                  <div className="content">
                    <div className="icons">
-                      <a href="https://github.com/josephambe" className="icon fa-github alt"><span className="label">GitHub</span></a>< br/>
-                      <a href="mailto:jamber919@gmail.com?subject=Let's catch up!" className="icon fa-envelope alt"><span className="label">Mail</span></a>< br/>
+                     <a href="mailto:jamber919@gmail.com?subject=Let's catch up!" className="icon fa-envelope alt"><span className="label">Mail</span></a>< br/>
+                     <a href="https://github.com/josephambe" className="icon fa-github alt"><span className="label">GitHub</span></a>< br/>
                       <a href="https://www.linkedin.com/in/amber-joseph/" className="icon fa-linkedin alt"><span className="label">LinkedIn</span></a>
                     </div>
                  </div>
