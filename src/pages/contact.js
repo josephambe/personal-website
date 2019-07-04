e0b742832e64842e29e9f8889621497a06c5ee00
Amber Joseph <jamber919@gmail.com>
2019-07-05 11:34:20 +1200

    Added and resized icons correctly for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,25 +18,23 @@ class Contact extends React.Component {
 
         <HeaderContact/>
 
-        <div id="projectList">
-          <section id="content" className="projectList">
-            <div className="grid-container">
+        <div id="contactGrid">
+          <section id="content" className="contactGrid">
                <div className="row">
-                <div className="project-item">
+                <div className="grid-item">
                  <div className="content">
-                    <ul className="icons">
-                      <li><a href="https://github.com/josephambe" className="icon fa-github alt"><span className="label">GitHub</span></a></li>< br/>
-                      <li><a href="mailto:jamber919@gmail.com?subject=Let's catch up!" className="icon fa-envelope alt"><span className="label">Mail</span></a></li>< br/>
-                      <li><a href="https://www.linkedin.com/in/amber-joseph/" className="icon fa-linkedin alt"><span className="label">LinkedIn</span></a></li>
-                    </ul>
+                   <div className="icons">
+                      <a href="https://github.com/josephambe" className="icon fa-github alt"><span className="label">GitHub</span></a>< br/>
+                      <a href="mailto:jamber919@gmail.com?subject=Let's catch up!" className="icon fa-envelope alt"><span className="label">Mail</span></a>< br/>
+                      <a href="https://www.linkedin.com/in/amber-joseph/" className="icon fa-linkedin alt"><span className="label">LinkedIn</span></a>
+                    </div>
                  </div>
                 </div>
-                <div className="project-item">
+                <div className="grid-item">
                   <div className="content">
                     <img src={pic03} alt="" />
                   </div>
                 </div>
-               </div>
             </div>
           </section>
         </div>
