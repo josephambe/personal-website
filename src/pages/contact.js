43a77ddf10fe8f61bb90fdec58747a16d1050305
Amber Joseph <jamber919@gmail.com>
2019-07-05 12:07:17 +1200

    Added map to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,8 +3,7 @@ import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import HeaderContact from '../components/HeaderContact'
-import pic04 from '../assets/images/pic04.jpg'
-import pic03 from '../assets/images/pic03.jpg'
+
 
 class Contact extends React.Component {
 
@@ -21,6 +20,7 @@ class Contact extends React.Component {
         <div id="contactGrid">
           <section id="content" className="contactGrid">
                <div className="row">
+
                 <div className="grid-item">
                  <div className="content">
                    <div className="icons">
@@ -30,11 +30,17 @@ class Contact extends React.Component {
                     </div>
                  </div>
                 </div>
+
                 <div className="grid-item">
                   <div className="content">
-                    <img src={pic03} alt="" />
+                    <div id="map-container-google-1" className="z-depth-1-half map-container" style={{height: "100%"}}>
+                      <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8134.1810411581855!2d174.77353115793437!3d-41.28978125245025!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d38afd36fddefd1%3A0x387378be6038d3c4!2sWellington+Waterfront!5e0!3m2!1sen!2snz!4v1562284979379!5m2!1sen!2snz"
+                        frameBorder="0" style={{border:"0"}} allowFullScreen></iframe>
+                    </div>
                   </div>
                 </div>
+                 
             </div>
           </section>
         </div>
