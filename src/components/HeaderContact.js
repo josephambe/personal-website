f3c5e2cae48ddb6efb24050fd9d898dff39d963f
Amber Joseph <jamber919@gmail.com>
2019-07-05 12:21:04 +1200

    Added title to HeaderContact

--- src/components/HeaderContact.js
+++ src/components/HeaderContact.js
@@ -9,7 +9,8 @@ const HeaderContact = (props) => (
           <img className="home-logo" src={home_duck} alt=""/>
         </a>
       </figure>
-      <h1>FIKA</h1>
+      <h3 style={{display: "inline", float: "left", paddingRight: "2%", paddingLeft: "27%"}}> Let's catch up for</h3>
+      <h1 style={{display: "inline", float: "left"}}>FIKA</h1>< br/>< br />< br/>
         <h2>(noun or verb)   /fi:ka/ </h2>
         <h3>A Swedish concept which means making time for friends and colleagues to share a cup of coffee (or tea) and a little something to eat. Seems to suit Wellington pretty well. </h3>
     </header>
