2d221eef555ed5124633f54a5cb15f51029642d5
Amber Joseph <jamber919@gmail.com>
2019-07-06 20:46:27 +1200

    Added painting content

--- src/pages/spotted.js
+++ src/pages/spotted.js
@@ -11,6 +11,8 @@ import pmsa from '../assets/images/spotted/pmsa/pic01.jpg'
 import mural from '../assets/images/spotted/mural/mural03.jpg'
 import sculpture from '../assets/images/spotted/sculpture/fruitMaking02.jpg'
 import artBoard from '../assets/images/spotted/art_board/painting06.jpg'
+import painting from '../assets/images/spotted/painting/leaves.jpg'
+
 
 import pic05 from '../assets/images/pic05.jpg'
 
@@ -64,8 +66,8 @@ class Spotted extends React.Component {
             </div>
             <div className="grid-item">
               <div className="content">
-                <img src={pic05} alt="" />
-                <overlay><button><a href="#" className="icon" title="User Profile">SINGAPORE</a></button></overlay>
+                <img src={painting} alt="" />
+                <overlay><a href="./blog_painting" className="icon" title="User Profile"><button>Painting</button></a></overlay>
               </div>
             </div>
           </div>
