edb3ae5eb5735082152b6d4806ae9071e39317b0
Amber Joseph <jamber919@gmail.com>
2019-07-07 07:40:01 +1200

    Added webstock content:

--- src/pages/spotted.js
+++ src/pages/spotted.js
@@ -12,6 +12,7 @@ import mural from '../assets/images/spotted/mural/mural03.jpg'
 import sculpture from '../assets/images/spotted/sculpture/fruitMaking02.jpg'
 import artBoard from '../assets/images/spotted/art_board/painting06.jpg'
 import painting from '../assets/images/spotted/painting/leaves.jpg'
+import webstock from '../assets/images/spotted/webstock/webstock02.jpg'
 
 
 import pic05 from '../assets/images/pic05.jpg'
@@ -68,6 +69,12 @@ class Spotted extends React.Component {
               <div className="content">
                 <img src={painting} alt="" />
                 <overlay><a href="./blog_painting" className="icon" title="User Profile"><button>Painting</button></a></overlay>
+              </div>
+            </div>
+            <div className="grid-item">
+              <div className="content">
+                <img src={webstock} alt="" />
+                <overlay><a href="./blog_webstock" className="icon" title="User Profile"><button>Webstock 2019</button></a></overlay>
               </div>
             </div>
           </div>
